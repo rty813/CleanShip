b7f18676dd4b04cc8ee9a10233debd3246e2eb7b
rty813 <523213189@qq.com>
2017-11-27 13:35:26 +0800

    创建了地图，实现了画点以及撤销的功能

--- app/src/main/java/com/xyz/rty813/cleanship/MainActivity.java
+++ app/src/main/java/com/xyz/rty813/cleanship/MainActivity.java
@@ -2,34 +2,61 @@ package com.xyz.rty813.cleanship;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Toast;
 
 import com.amap.api.maps2d.AMap;
 import com.amap.api.maps2d.MapView;
 import com.amap.api.maps2d.model.LatLng;
+import com.amap.api.maps2d.model.Marker;
+import com.amap.api.maps2d.model.MarkerOptions;
 import com.amap.api.maps2d.model.MyLocationStyle;
+import com.amap.api.maps2d.model.Polyline;
+import com.amap.api.maps2d.model.PolylineOptions;
 
-public class MainActivity extends AppCompatActivity implements AMap.OnMapClickListener{
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements AMap.OnMapClickListener, AMap.OnMarkerClickListener{
     private MapView mMapView;
+    private AMap aMap;
+    private ArrayList<Marker> markers;
+    private ArrayList<Polyline> polylines;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mMapView = findViewById(R.id.mapview);
         mMapView.onCreate(savedInstanceState);
-
-        AMap aMap = null;
+        markers = new ArrayList<>();
+        polylines = new ArrayList<>();
         if (aMap == null){
             aMap = mMapView.getMap();
         }
 
         MyLocationStyle myLocationStyle = new MyLocationStyle();
-//        myLocationStyle.interval(2000);
         myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATE);
         aMap.setMyLocationStyle(myLocationStyle);
         aMap.setMyLocationEnabled(true);
+        aMap.setOnMarkerClickListener(this);
         aMap.setOnMapClickListener(this);
+        findViewById(R.id.btn_cancel).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (markers.size() > 0) {
+                    Marker marker = markers.get(markers.size() - 1);
+                    marker.hideInfoWindow();
+                    markers.get(markers.size() - 1).destroy();
+                    markers.remove(markers.size() - 1);
+                    if (polylines.size() > 0) {
+                        polylines.get(polylines.size() - 1).remove();
+                        polylines.remove(polylines.size() - 1);
+                    }
+                }
+            }
+        });
     }
 
     @Override
@@ -58,7 +85,33 @@ public class MainActivity extends AppCompatActivity implements AMap.OnMapClickLi
 
     @Override
     public void onMapClick(LatLng latLng) {
-        Toast.makeText(this, latLng.toString(), Toast.LENGTH_SHORT).show();
-        System.out.println(latLng.toString());
+//        System.out.println(latLng.toString());
+        Marker marker = aMap.addMarker(new MarkerOptions().position(latLng).title(String.valueOf(markers.size() + 1)).snippet("Lat:" + latLng.latitude + "\nLng:" + latLng.longitude));
+        markers.add(marker);
+        marker.showInfoWindow();
+        if (markers.size() > 1) {
+            LatLng latLng1 = markers.get(markers.size() - 2).getPosition();
+            LatLng latLng2 = markers.get(markers.size() - 1).getPosition();
+            polylines.add(aMap.addPolyline(new PolylineOptions().add(latLng1, latLng2).width(7)));
+        }
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+//        Toast.makeText(this,"H", Toast.LENGTH_SHORT).show();
+        if (marker.isInfoWindowShown()){
+            marker.hideInfoWindow();
+        }
+        else{
+            marker.showInfoWindow();
+        }
+        if ((markers.size() > 1) && (marker.getTitle().equals("1")) && (!markers.get(markers.size() - 1).getTitle().equals("1"))){
+            LatLng latLng = markers.get(markers.size() - 1).getPosition();
+            markers.add(aMap.addMarker(new MarkerOptions().position(marker.getPosition())));
+            polylines.add(aMap.addPolyline(new PolylineOptions().add(latLng, marker.getPosition()).width(7)));
+//            Toast.makeText(this, "完成闭合回路！", Toast.LENGTH_SHORT).show();
+            Snackbar.make(findViewById(R.id.root_view), "完成闭合回路", Snackbar.LENGTH_SHORT).show();
+        }
+        return true;
     }
 }
